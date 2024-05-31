f5e1864200f7ef96add84a76aed0eb608687b6c2
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-06-01 00:14:40 +0530

    Added initial route

--- app.py
+++ app.py
@@ -12,12 +12,18 @@ model = load_model('./model_VGG16_23mei.h5')
 # Ensure model is ready to make predictions
 model.make_predict_function()
 
+
 def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(300, 300))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0
     return img_array
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
