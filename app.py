c748b79f845cced962b9d17d278a2d9972bc29e3
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-06-01 00:13:58 +0530

    Preprocess the image

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ model = load_model('./model_VGG16_23mei.h5')
 # Ensure model is ready to make predictions
 model.make_predict_function()
 
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(300, 300))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+    return img_array
+
 
 if __name__ == "__main__":
     app.run(debug=True)
